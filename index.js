12a90a894b8b4bf404479faddd9540043280eba7
krishpranav <krisna.pranav@gmail.com>
2021-04-17 18:28:01 +0530

    added main index file

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ const youtubedl = require("ytdl-core");
 const openPip = require("open-pip");
 
 const getInfo = promisify(youtubedl.getInfo);
+
+xns(async () => {
+    const info = await getInfo(process.argv[2]);
+    const format = youtubedl.chooseFormat(info.formats, {});
+    await openPip(format.url);
+    await new Promise(resolve => setTimeout(resolve, 3000));
+});
